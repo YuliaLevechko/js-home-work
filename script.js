883b2f45aaa0abf2bb02b3d70dc69d09198fdce4
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2023-06-06 20:07:30 +0300

    added task 1-6

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
 //task-1
-const productName = "Droid";
+/*const productName = "Droid";
 console.log(productName);
 const pricePerItem = 2000;
-console.log(pricePerItem);
+console.log(pricePerItem);*/
+
+//task-2
+/*let productName = "Droid";
+let pricePerItem = 2000;
+productName = "Repair droid";
+pricePerItem += 1500;
+
+console.log(productName);
+console.log(pricePerItem);*/
+
+//task-3
+/*const topSpeed = 160;
+const distance = 617.54;
+const login = "mango935";
+const isOnline = true;
+const isAdmin = false;
+
+console.log(topSpeed);
+console.log(distance);
+console.log(login);
+console.log(isOnline);
+console.log(isAdmin);*/
+
+//task-4
+/*const pricePerItem = 3500;   // ціна за одиницю
+const orderedQuantity = 4;   //замовлена кількість
+
+const totalPrice = pricePerItem * orderedQuantity;
+console.log(totalPrice);*/
+
+//task-5
+/*const productName = "Droid";
+const pricePerItem = 3500;
+
+const message = `You picked ${productName}, price per item is ${pricePerItem} credits`
+console.log(message);*/
+
+//task-6
+/*const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
+const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+console.log(message);*/
+
+//task-7
 
 
