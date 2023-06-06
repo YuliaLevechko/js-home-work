150d0739aab35786bb92966bd0a22018b7358008
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2023-06-06 22:49:13 +0300

    add  task 7-12

--- script.js
+++ script.js
@@ -1,53 +1,90 @@
 //task-1
-/*const productName = "Droid";
-console.log(productName);
-const pricePerItem = 2000;
-console.log(pricePerItem);*/
+// const productName = "Droid";
+// console.log(productName);
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
 
 //task-2
-/*let productName = "Droid";
-let pricePerItem = 2000;
-productName = "Repair droid";
-pricePerItem += 1500;
-
-console.log(productName);
-console.log(pricePerItem);*/
+// let productName = "Droid";
+// let pricePerItem = 2000;
+// productName = "Repair droid";
+// pricePerItem += 1500;
+// console.log(productName);
+// console.log(pricePerItem);
 
 //task-3
-/*const topSpeed = 160;
-const distance = 617.54;
-const login = "mango935";
-const isOnline = true;
-const isAdmin = false;
-
-console.log(topSpeed);
-console.log(distance);
-console.log(login);
-console.log(isOnline);
-console.log(isAdmin);*/
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// console.log(isOnline);
+// console.log(isAdmin);
 
 //task-4
-/*const pricePerItem = 3500;   // ціна за одиницю
-const orderedQuantity = 4;   //замовлена кількість
-
-const totalPrice = pricePerItem * orderedQuantity;
-console.log(totalPrice);*/
+// const pricePerItem = 3500;   // ціна за одиницю
+// const orderedQuantity = 4;   //замовлена кількість
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
 
 //task-5
-/*const productName = "Droid";
-const pricePerItem = 3500;
-
-const message = `You picked ${productName}, price per item is ${pricePerItem} credits`
-console.log(message);*/
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`
+// console.log(message);
 
 //task-6
-/*const pricePerDroid = 800;
-const orderedQuantity = 6;
-const deliveryFee = 50;
-const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
-const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
-console.log(message);*/
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = (pricePerDroid * orderedQuantity) + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+// console.log(message);
 
 //task-7
+// function sayHi() {
+//     console.log("Hello, this is my first function!");
+// }
+// sayHi();
 
+//task-8
+// function add(a, b, c) {
+//     console.log(`Addition result equals ${a + b + c}`);
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
 
+//task-9
+// function add(a, b, c) {
+//     return a + b + c;
+// }
+// add(2, 5, 8);
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+//task-10
+// function makeMessage(name, price) {
+//    const message = `You picked ${name}, price per item is ${price} credits`;
+//    return message;
+// };
+// makeMessage("Radar", 6150);
+// makeMessage("Scanner", 3500);
+// makeMessage("Reactor", 8000);
+// makeMessage("Engine", 4070);
+
+//task-11                         //(кількість,  ціна )
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   const totalPrice = orderedQuantity * pricePerItem;
+
+//     return totalPrice;
+// };
+// calculateTotalPrice(5, 100);
+// calculateTotalPrice(8, 60);
+// calculateTotalPrice(3, 400);
+// calculateTotalPrice(1, 3500);
+// calculateTotalPrice(12, 70);
